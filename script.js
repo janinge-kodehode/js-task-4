e7b8d865df0ddb3b4e4f3eb2d5650df1d274f2f2
janinge-kodehode <janinge.kodehode@gmail.com>
2025-08-10 20:50:01 +0200

    ok now finito

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const display = document.getElementById('display');
 
+//Takes the input and appends it to the display
 function appendToDisplay(input) {
   display.value += input;
 }
 
+//Clears the display
 function clearDisplay() {
   display.value = '';
 }
 
+//Calculates the equation in the display, if the equiation isnt valid it returns an error message, If the display is empty it returns undefined
 function calculate() {
   try {
     display.value = eval(display.value);
